7c5b351d848f21c5dbbe0dd63e14a2fe18187aa9
Jeremy Hamilton <jeremyhamilton035@gmail.com>
2019-08-13 13:15:33 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,11 +1,8 @@
-class Song 
-  
-  
-  
-  
-  
-  
-  
-  
-  
+class Song
+  attr_accessor :artist, :name, :genre
+ 
+  def initialize(name, genre)
+    @name = name
+    @genre = genre
+  end
 end
