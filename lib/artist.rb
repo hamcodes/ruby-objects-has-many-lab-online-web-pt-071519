f81fa6c88302b9af7170270166770fe6193b4968
Jeremy Hamilton <jeremyhamilton035@gmail.com>
2019-08-13 13:06:10 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -10,13 +10,13 @@ class Artist
 
 
 
-    def add_song(song)
-    @songs << song
-  end
+  #   def add_song(song)
+  #   @songs << song
+  # end
  
-  def songs
-    @songs
-  end
+  # def songs
+  #   @songs
+  # end
   
   
   
