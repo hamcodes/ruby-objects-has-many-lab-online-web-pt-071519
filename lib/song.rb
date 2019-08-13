196e66fcaf43c85602021a07a8b1781c3adeec62
Jeremy Hamilton <jeremyhamilton035@gmail.com>
2019-08-13 13:42:35 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,15 +1,31 @@
-class Song 
-  attr_accessor :name, :artist 
+class Song
 
-  @@all = []
+   @@all = []
 
-  def initialize(name, artist=nil)
+   attr_accessor :name, :artist
+
+   def initialize(name)
     @name = name
-    @artist = artist
     @@all << self
+  end
+
+   def self.all
+    @@all
+  end
+
+   def artist_name
+  if @artist
+    self.artist.name
+  else
+    @artist
   end
 end
 
+ def self.song_count
+@@all.uniq.lenght
+end
+
+ end
 
 
 
