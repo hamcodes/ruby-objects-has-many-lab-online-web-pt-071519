63c73183fd5ef54b1c85b92f94d3598577086e44
Jeremy Hamilton <jeremyhamilton035@gmail.com>
2019-08-13 13:13:52 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,7 +1,17 @@
 class Artist
   attr_accessor :name
-
-   
+ 
+  def initialize(name)
+    @name = name
+  end
+ 
+  def add_song(song)
+    song.artist = self
+  end
+ 
+  def songs
+    Song.all.select {|song| song.artist == self}
+  end
 end
   
   
